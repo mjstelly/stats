554325a48bd783f67da486d4fa83519fd3138383
Michael Stelly <bulletproofapps@pm.me>
2021-09-19 17:33:03 -0500

    initial commit

---
+++ src/CsvFileReader.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import { MatchResult } from './MatchResults';
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export abstract class CsvFileReader {
+
+  data: MatchData[] = [];
+
+  constructor(public fileName: string) { }
+  abstract mapRow(row: string[]): MatchData;
+
+  read(): void {
+    this.data = fs
+      .readFileSync(this.fileName, {
+        encoding: 'utf-8'
+
+      }).split('\n')
+      .map((row: string): string[] => {
+        return row.split(',');
+      })
+      .map(this.mapRow);
+  }
+}
